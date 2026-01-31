6c119cd246ee336b74a3ec76399ded09748fe805
git2hub980 <useless311980@gmail.com>
2026-02-01 02:12:58 +0530

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,24 @@
-console.log("Frontend loaded");
+function goToStep2() {
+    document.getElementById("step1").classList.add("hidden");
+    document.getElementById("step2").classList.remove("hidden");
+}
+
+function goToStep3() {
+    document.getElementById("step2").classList.add("hidden");
+    document.getElementById("step3").classList.remove("hidden");
+}
+
+function startFlashTest() {
+    alert("Place your finger on camera & flash.");
+    // Later: camera + PPG logic
+}
+
+function startIMUTest() {
+    alert("Place phone on stomach for 30 seconds.");
+    // Later: motion sensor logic
+}
+
+function submitAll() {
+    document.getElementById("result").innerText =
+        "Data submitted. AI is analyzing your risk...";
+}
